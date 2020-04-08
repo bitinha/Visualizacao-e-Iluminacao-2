6d7fb8912781cd8764bd4ae94e8d51a9d5a9198b
Vítor Domingos Araújo Gomes <vitordagomes@outlook.com>
2020-04-08 19:29:16 +0100

    Remoção do anti-aliasing

--- TP2/optix/TP2.cu
+++ TP2/optix/TP2.cu
@@ -57,13 +57,13 @@ extern "C" __global__ void __closesthit__radiance()
     float3 btz, tz;    
 
 
-    btx = make_float3(-normal.y,normal.x,0);
-    tx  = cross( btx , make_float3(normal));
+    btx = normalize(make_float3(-normal.y,normal.x,0));
+    tx  = normalize(cross( btx , make_float3(normal)));
     
 
   
-    btz = make_float3(0,normal.z,-normal.y);
-    tz  = cross( btz , make_float3(normal));
+    btz = normalize(make_float3(0,normal.z,-normal.y));
+    tz  = normalize(cross( btz , make_float3(normal)));
 
 
     for (int i = 0; i < raysPerPixel; i++){
@@ -93,13 +93,12 @@ extern "C" __global__ void __closesthit__radiance()
             
 
             float3 p = x * bt + y * make_float3(normal) + z * t;
-            //p = normalize(p);
 
             optixTrace(optixLaunchParams.traversable,
              make_float3(pos),
              p,
-             0.001f,  // tmin
-             0.40f, // tmax
+             0.1f,//0.001f,  // tmin
+             100.f,//0.40f, // tmax
              0.0f, // rayTime
              OptixVisibilityMask( 255 ),
              OPTIX_RAY_FLAG_DISABLE_ANYHIT,
@@ -135,6 +134,56 @@ extern "C" __global__ void __miss__radiance() {
 // ray gen program - responsible for launching primary rays
 extern "C" __global__ void __raygen__renderFrame() {
 
+
+    const int ix = optixGetLaunchIndex().x;
+    const int iy = optixGetLaunchIndex().y;
+    const auto &camera = optixLaunchParams.camera;  
+    
+    // ray payload
+    float3 pixelColorPRD = make_float3(1.f);
+    uint32_t u0, u1;
+    packPointer( &pixelColorPRD, u0, u1 );  
+
+    // compute ray direction
+    // normalized screen plane position, in [-1, 1]^2
+    const float2 screen(make_float2(ix+.5f,iy+.5f)
+                    / make_float2(optixGetLaunchDimensions().x, optixGetLaunchDimensions().y) * 2.0 - 1.0);
+  
+    // note: nau already takes into account the field of view when computing 
+    // camera horizontal and vertival
+    float3 rayDir = normalize(camera.direction
+                           + screen.x  * camera.horizontal
+                           + screen.y * camera.vertical);
+    
+    // trace primary ray
+    optixTrace(optixLaunchParams.traversable,
+             camera.position,
+             rayDir,
+             0.f,    // tmin
+             1e20f,  // tmax
+             0.0f,   // rayTime
+             OptixVisibilityMask( 255 ),
+             OPTIX_RAY_FLAG_DISABLE_ANYHIT,
+             PHONG_RAY_TYPE,             // SBT offset
+             RAY_TYPE_COUNT,             // SBT stride
+             PHONG_RAY_TYPE,             // missSBTIndex 
+             u0, u1 );
+
+    //convert float (0-1) to int (0-255)
+    const int r = int(255.0f*pixelColorPRD.x);
+    const int g = int(255.0f*pixelColorPRD.y);
+    const int b = int(255.0f*pixelColorPRD.z);
+
+    // convert to 32-bit rgba value 
+    const uint32_t rgba = 0xff000000
+      | (r<<0) | (g<<8) | (b<<16);
+    // compute index
+    const uint32_t fbIndex = ix+iy*optixGetLaunchDimensions().x;
+    // write to output buffer
+    optixLaunchParams.frame.colorBuffer[fbIndex] = rgba;
+
+
+/*
     // compute a test pattern based on pixel ID
     const int ix = optixGetLaunchIndex().x;
     const int iy = optixGetLaunchIndex().y;
@@ -219,6 +268,7 @@ extern "C" __global__ void __raygen__renderFrame() {
     const uint32_t fbIndex = ix + iy*optixGetLaunchDimensions().x;
     // write to output buffer
     optixLaunchParams.frame.colorBuffer[fbIndex] = rgba;
+*/
 }
 
 

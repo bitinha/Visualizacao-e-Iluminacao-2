01ab61007bc837080e4ca80d7c4c732def2b8795
Joel Gama <joelgama2015@gmail.com>
2020-05-09 19:07:07 +0100

    Agumas correções nos pontos pretos.

--- TP3/optix12-DepthOfField/optix.cu
+++ TP3/optix12-DepthOfField/optix.cu
@@ -24,10 +24,22 @@ struct shadowPRD{
 // -------------------------------------------------------
 // closest hit computes color based lolely on the triangle normal
 
-extern "C" __global__ void __closesthit__radiance() {
+extern "C" __global__ void __closesthit__radiance()
+{
 
-    colorPRD &prd = *(colorPRD *)getPRD<colorPRD>();
+    float3 pixelColorPRD = make_float3(1.f);
+    uint32_t u0, u1;
+    packPointer( &pixelColorPRD, u0, u1 );  
 
+    float4 lightPos = optixLaunchParams.global->lightPos;
+    
+    // get the payload variable
+    float3 &prd = *(float3*)getPRD<float3>();
+
+    const int ix = optixGetLaunchIndex().x;
+    const int iy = optixGetLaunchIndex().y;
+
+    // get mesh data
     const TriangleMeshSBTData &sbtData
       = *(const TriangleMeshSBTData*)optixGetSbtDataPointer();  
 
@@ -39,113 +51,108 @@ extern "C" __global__ void __closesthit__radiance() {
     const float u = optixGetTriangleBarycentrics().x;
     const float v = optixGetTriangleBarycentrics().y;
 
-    // compute normal
-    const float4 n
-        = (1.f-u-v) * sbtData.vertexD.normal[index.x]
-        +         u * sbtData.vertexD.normal[index.y]
-        +         v * sbtData.vertexD.normal[index.z];
+    if (sbtData.hasTexture && sbtData.vertexD.texCoord0) {
 
-    // intersection position
-    const float3 pos = optixGetWorldRayOrigin() + optixGetRayTmax()*optixGetWorldRayDirection();
-
-    // direction towards light
-    float3 lPos = make_float3(optixLaunchParams.global->lightPos);
-    float3 lDir = normalize(lPos - pos);
-    float3 nn = normalize(make_float3(n));
-    float intensity = max(dot(lDir, nn),0.0f);
-    
-
-    // ray payload
-    shadowPRD shadowAttPRD;
-    shadowAttPRD.shadowAtt = 1.0f;
-    shadowAttPRD.seed = prd.seed;
-    uint32_t u0, u1;
-    packPointer( &shadowAttPRD, u0, u1 );  
-  
-    // trace shadow ray
-    int squaredShadowRays = 1;
-    float shadowTotal = 0.0f;
-    for (int i = 0; i < squaredShadowRays; ++i) {
-        for (int j = 0; j < squaredShadowRays; ++j) {
-
-            //uint32_t seed = tea<4>(  , i * squaredShadowRays + j );
-
-            //const float2 subpixel_jitter = make_float2( i * delta.x + delta.x *  rnd( seed ), j * delta.y + delta.y * rnd( seed ) );
-            //const float2 subpixel_jitter = make_float2( rnd( seed )-0.5f, rnd( seed )-0.5f );
-            lPos.x = -0.2 + i * 1.0/squaredShadowRays * 0.4f + rnd(prd.seed) * 1.0/squaredShadowRays * 0.4;
-            lPos.z = -0.2 + j * 1.0/squaredShadowRays * 0.4f + rnd(prd.seed) * 1.0/squaredShadowRays * 0.4;
-            lDir = normalize(lPos - pos);
-            optixTrace(optixLaunchParams.traversable,
-                pos,
-                lDir,
-                0.00001f,           // tmin
-                10,                 // tmax
-                0.0f,               // rayTime
-                OptixVisibilityMask( 255 ),
-                OPTIX_RAY_FLAG_NONE, //OPTIX_RAY_FLAG_NONE,
-                SHADOW,             // SBT offset
-                RAY_TYPE_COUNT,     // SBT stride
-                SHADOW,             // missSBTIndex 
-                u0, u1 );
-
-                shadowTotal += shadowAttPRD.shadowAtt;
-        }
-    }
-    shadowTotal /= (squaredShadowRays * squaredShadowRays);
-
-    if (sbtData.hasTexture && sbtData.vertexD.texCoord0) {  
-        // get barycentric coordinates
         // compute pixel texture coordinate
         const float4 tc
           = (1.f-u-v) * sbtData.vertexD.texCoord0[index.x]
           +         u * sbtData.vertexD.texCoord0[index.y]
           +         v * sbtData.vertexD.texCoord0[index.z];
         // fetch texture value
-        
         float4 fromTexture = tex2D<float4>(sbtData.texture,tc.x,tc.y);
-        prd.color = make_float3(fromTexture) * min(intensity * shadowTotal + 0.0, 1.0);
+        prd= make_float3(fromTexture);
+    
     }
     else
-        prd.color = sbtData.color * min(intensity * shadowTotal + 0.0, 1.0);
+        prd = sbtData.color;
+
+    // Normal do pixel
+    float4 normal = (1.f-u-v) * sbtData.vertexD.normal[index.x]
+        +         u * sbtData.vertexD.normal[index.y]
+        +         v * sbtData.vertexD.normal[index.z];
+
+
+    // I=L•N*C*I_l
+    float intensidade = max(dot(normalize(lightPos),normalize(normal)),0.f) ;
+
+
+    float4 pos
+        = (1.f-u-v) * sbtData.vertexD.position[index.x]
+        +         u * sbtData.vertexD.position[index.y]
+        +         v * sbtData.vertexD.position[index.z];
+
+    optixTrace(optixLaunchParams.traversable,
+                make_float3(pos),
+                make_float3(lightPos),
+                0.001f, // tmins
+                1e20f, // tmax
+                0.0f, // rayTime
+                OptixVisibilityMask( 255 ),
+                OPTIX_RAY_FLAG_DISABLE_ANYHIT,
+                SHADOW, // SBT offset
+                RAY_TYPE_COUNT, // SBT stride
+                SHADOW, // missSBTIndex
+                u0, u1 );   
+
+
+    const auto &camera = optixLaunchParams.camera; 
+    
+    float3 specularColor = make_float3(0,0,0);
+
+    float3 vertexToEye = normalize(make_float3(pos)-camera.position);
+    float3 lightReflect = make_float3(normalize(reflect(-lightPos,normal)));
+    float specularFactor = dot(vertexToEye,lightReflect);
+    if (specularFactor > 0) {
+        int shininess = 1;
+        specularFactor = pow(specularFactor,shininess);
+        specularColor = pixelColorPRD * 1 * specularFactor;
+    }
+
+    // Componente ambiente + componente difusa*intensidade + componente especular
+    prd = (prd*0.2 + prd*pixelColorPRD*(0.6) * intensidade)  + specularColor*0.2;
+
 }
 
-
-// any hit to ignore intersections with back facing geometry
+// nothing to do in here
 extern "C" __global__ void __anyhit__radiance() {
-
 }
 
 
 // miss sets the background color
 extern "C" __global__ void __miss__radiance() {
 
-    colorPRD &prd = *(colorPRD*)getPRD<colorPRD>();
+    float3 &prd = *(float3*)getPRD<float3>();
     // set blue as background color
-    prd.color = make_float3(0.0f, 0.0f, 1.0f);
+    prd = make_float3(0.0f, 0.7f, 1.0f);
 }
 
 
 // -----------------------------------------------
 // Shadow rays
 
-extern "C" __global__ void __closesthit__shadow() {
-
-    shadowPRD &prd = *(shadowPRD*)getPRD<shadowPRD>();
-    prd.shadowAtt = 0.0f;
-}
-
-
-// any hit for shadows
+// nothing to do in here
 extern "C" __global__ void __anyhit__shadow() {
-
+    /*
+    float3 &prd = *(float3*)getPRD<float3>();
+    // set the shadow color
+    prd = make_float3(0.0f, 0.0f, 0.0f);
+    */
+}
+  
+// nothing to do in here
+extern "C" __global__ void __closesthit__shadow() {
+    float3 &prd = *(float3*)getPRD<float3>();
+    // set the shadow color
+    prd = make_float3(0.0f, 0.0f, 0.0f);
 }
 
-
-// miss for shadows
+// miss sets the background color
 extern "C" __global__ void __miss__shadow() {
-
-    shadowPRD &prd = *(shadowPRD*)getPRD<shadowPRD>();
-    prd.shadowAtt = 1.0f;
+    
+    float3 &prd = *(float3*)getPRD<float3>();
+    // set the background color
+    prd = make_float3(1.0f, 1.0f, 1.0f);
+    
 }
 
 
